a4932fe67dd1adf2fb39a4a793318ddd727a6e5c
muzucode <79173451+muzucode@users.noreply.github.com>
2021-08-04 14:03:30 -0400

    init amplify app

--- myapp/config.js
+++ myapp/config.js
@@ -2,7 +2,7 @@ window._config = {
   cognito: {
     userPoolId: 'us-east-1_H21vtvGaV', // e.g. us-east-2_uXboG5pAb
     userPoolClientId: '2fegv8929b7lpdpf7uj9vsp2s7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: 'us-east-' // e.g. us-east-2
+    region: 'us-east-1' // e.g. us-east-2
   },
   api: {
       invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
