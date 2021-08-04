72d4f9e9c4060e1793a13a6e6e60062afafbb370
muzucode <79173451+muzucode@users.noreply.github.com>
2021-08-03 23:53:51 -0400

    build commit

---
+++ myapp/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container  mt-3 ">
+  <div class="container bg-light shadow p-5">
+    <h1 class="display-4 mb-5">Rene Descartes Portrait</h1>
+    <img class="mb-5" src="https://freepikpsd.com/media/2019/10/descartes-png-3-Transparent-Images.png"/>
+    <p>A still of the legendary philosopher, Rene Descartes</p>
+    <p><b>Rarity: 76</b></p>
+    <p><i>265,876 in circulation</i></p>
+    <p><i>Item ID: Fbsh46Fhfcb24</i></p>
+
+  </div>
+  <button type="button" class="btn btn-lg btn-secondary mt-5 w-25">Generate</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
