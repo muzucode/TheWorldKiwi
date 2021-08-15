fb85992a5d342d4c62ddb258ec85c4947ad009e2
muzucode <79173451+muzucode@users.noreply.github.com>
2021-08-15 10:04:44 -0400

    added number gen and api methods

---
+++ myapp/src/services/AmplifyAPIService.js
@@ -0,0 +1,85 @@
+import {API} from 'aws-amplify';
+
+const apiName = 'StrawberryAPI';
+/* eslint-disable */
+const myInit = { // OPTIONAL
+  headers: {}, // OPTIONAL
+  response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+  queryStringParameters: {  // OPTIONAL
+      name: 'param',
+  },
+};
+
+class AmplifyAPIService {
+
+  // Sample get request
+  getReq(){
+    const response = API.get(apiName, '/items')
+    .then(res => {
+      // Add your code here
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error.response);
+   });
+
+   return response
+  }
+
+  // Sample get request
+  getWorks(){
+    const response = API.get(apiName, '/works')
+    .then(res => {
+      // Add your code here
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
+
+    return response
+  }
+
+  postWork(){
+    const response = API.post(apiName, '/works/generate')
+    .then(res => {
+      // Add your code here
+      console.log('yo');
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error.response);
+   });
+
+   return response
+  }
+
+  generateWork(){
+    const response = API.get(apiName, '/works/generate')
+    .then(res => {
+      // Add your code here
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error.response);
+   });
+
+   return response
+  }  
+  
+  generateItem(){
+    const response = API.get(apiName, '/items/generate')
+    .then(res => {
+      // Add your code here
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error.response);
+   });
+
+   return response
+  }
+}
+
+
+export default new AmplifyAPIService();
--- myapp/src/views/Home.vue
+++ myapp/src/views/Home.vue
@@ -1,24 +1,80 @@
 <template>
 <div class="container  mt-3 ">
   <MainCard>
-    <h1 class="display-4 mb-5">Rene Descartes Portrait</h1>
-    <img class="mb-5" src="https://freepikpsd.com/media/2019/10/descartes-png-3-Transparent-Images.png"/>
+    <!-- <h1 class="display-4 mb-5">Rene Descartes Portrait</h1> -->
+    <!-- <img class="mb-5" src="https://freepikpsd.com/media/2019/10/descartes-png-3-Transparent-Images.png"/>
     <p>A still of the legendary philosopher, Rene Descartes</p>
     <p><b>Rarity: 76</b></p>
     <p><i>265,876 in circulation</i></p>
-    <p><i>Item ID: Fbsh46Fhfcb24</i></p>
+    <p><i>Item ID: Fbsh46Fhfcb24</i></p> -->
+    <p class="display-2">{{winMessage}}</p>
+    <p class="display-7">{{rand}}</p>
+    
+
   </MainCard>
-  <button type="button" class="btn btn-lg btn-secondary mt-5 w-25">Generate</button>
+  <button @click="generateWork" type="button" class="btn btn-lg btn-secondary mt-5 w-25">Generate</button>
+  <button @click="getReq" type="button" class="btn btn-lg btn-secondary mt-5 w-25">Send get request</button>
+  <button @click="rollRand" type="button" class="btn btn-lg btn-secondary mt-5 w-25">Random #</button>
+
 </div>
 </template>
 
 <script>
-import MainCard from '../components/MainCard.vue'
+import MainCard from '../components/MainCard.vue';
+import AmplifyAPIService from '../services/AmplifyAPIService';
 
 export default {
   name: 'Home',
+  data: function () {
+    return {
+      title: '',
+      subtitle: '',
+      rarity: '',
+      circulation: 0,
+      id: '',
+      rand: 0,
+      winMessage: ''
+    }
+  },
   components: {
     MainCard
+  },
+  methods: {
+    getReq: function () {
+      AmplifyAPIService.getReq();
+    },
+    getWorks: function (){
+      AmplifyAPIService.getWorks();
+    },
+    generateWork: function (){
+      AmplifyAPIService.generateWork();
+    },
+    generateItem: function() {
+      AmplifyAPIService.generateItem();
+    },
+    setWinMessage: function(rand) {
+      // Generate random number
+      if(rand >= 0 && rand < 50) {
+        this.winMessage = 'Common';
+      } else if(rand >= 50 && rand < 70) {
+        this.winMessage = 'Uncommon';
+      } else if(rand >= 70 && rand < 85) {
+        this.winMessage = 'Rare';
+      } else if(rand >= 85 && rand < 95) {
+        this.winMessage = 'Epic';
+      } else if(rand >= 95 && rand <= 100) {
+        this.winMessage = 'Legendary';
+      } 
+    },
+    rollRand: function() {
+      // Generate random number
+      const rand = Math.round(Math.random(1) * 100);
+      this.rand = rand;
+      this.setWinMessage(rand);
+    }
+  },
+  created: function() {
+    this.rollRand();
   }
 }
 </script>
